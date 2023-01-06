35a4df106ee5ebad335e00b6f90adbc516182b5c
kaijia <kaijia@kaijia.com>
2023-01-06 11:41:20 +0800

    1.0.1

--- script/publish.ts
+++ script/publish.ts
@@ -1,10 +1,14 @@
 const execa = require("execa");
 import { resolve } from "path";
+import { version } from "../package.json";
 
 const cwd = resolve(__dirname, "../dist");
 
 const main = () => {
   try {
+    execa.commandSync(`git add .`, { stdio: "inherit" });
+    execa.commandSync(`git commit -am ${version}`, { stdio: "inherit" });
+
     execa.commandSync("npm publish", { cwd, stdio: "inherit" });
     execa.commandSync(`git push origin main`, { stdio: "inherit" });
     console.log("发布成功!!!");
--- script/pushGithub.ts
+++ script/pushGithub.ts
@@ -4,8 +4,8 @@ import { version } from "../package.json";
 const main = () => {
   execa.commandSync(`git add .`, { stdio: "inherit" });
   execa.commandSync(`git commit -am ${version}`, { stdio: "inherit" });
-
-  console.log(`git commit success  ${version}`);
+  execa.commandSync(`git push origin main`, { stdio: "inherit" });
+  console.log(`推送github成功  ${version}`);
 };
 
 main();
